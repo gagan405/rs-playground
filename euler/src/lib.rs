12fe114e3705b3c64b24aa7a13e4b4de5085d223
Gagan Mishra <gmishra@aerospike.com>
2025-11-07 21:48:44 -0800

    just a TODO

---
+++ euler/src/lib.rs
@@ -0,0 +1,9 @@
+pub fn split_number(number: u32) -> Vec<Vec<u32>> {
+
+    //TODO
+    fn split_number_recursively(number: u32, idx: u32, mut res: Vec<u32>) -> Vec<Vec<u32>> {
+        vec![]
+    }
+
+    vec![]
+}
